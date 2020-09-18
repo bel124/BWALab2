c5e300a4b3f1af77f851b40201d32306d62c1aab
bel124 <mferdinan124@hotmail.com>
2020-09-18 15:25:57 -0400

    fixed both files

--- lab2.js
+++ lab2.js
@@ -1,4 +1,12 @@
 function modifyInto() {
     document.getElementById("combo").innerHTML = "Wow, changed!";
+}
 
+function validate() {
+    var val = document.getElementById("number").value;
+
+    if(val >= 10 && val <= 15)
+        document.getElementById("output").textContent = "Input is OK"
+    else
+        document.getElementById("output").textContent = "Input is not valid"
 }
